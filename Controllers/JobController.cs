81747b16c439a81fa6c7a244281a27315ec07643
wormster <johnwormald@gmail.com>
2020-10-23 22:40:34 +0100

    Add project files.

---
+++ Controllers/JobController.cs
@@ -0,0 +1,62 @@
+﻿using ThePieShop.Data.Repositories;
+using ThePieShop.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ThePieShop.WebAPI.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : Controller
+    {
+        private readonly IJobRepository _jobRepository;
+
+        public JobController(IJobRepository jobRepository)
+        {
+            _jobRepository = jobRepository;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult GetJobs()
+        {
+            return Ok(_jobRepository.GetAllJobs());
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public IActionResult GetJobById(int id)
+        {
+            return Ok(_jobRepository.GetJobById(id));
+        }
+
+        [HttpPost]
+        public IActionResult CreateJob([FromBody] Job job)
+        {
+            if (job == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var createdJob = _jobRepository.AddJob(job);
+
+            return Created("Job", createdJob);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateJob([FromBody] Job updatedJob)
+        {
+            _jobRepository.UpdateJob(updatedJob);
+            return Ok(updatedJob);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteJob(int id)
+        {
+            _jobRepository.DeleteJob(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ThePieShop.Data.Repositories;
+
+namespace ThePieShop.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ICountryRepository, CountryRepository>();
+            services.AddScoped<IJobCategoryRepository, JobCategoryRepository>();
+            services.AddScoped<IJobRepository, JobRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IExpenseRepository, ExpenseRepository>();
+            services.AddScoped<ICurrencyRepository, CurrencyRepository>();
+            services.AddScoped<ITaskRepository, TaskRepository>();
+            services.AddScoped<ISurveyRepository, SurveyRepository>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+
+            services.AddControllers()
+                .ConfigureApiBehaviorOptions(options =>
+                {
+                    options.SuppressModelStateInvalidFilter = true;
+                    options.SuppressMapClientErrors = true;
+                })
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
+
+
+    //        services.AddControllers()
+    //.ConfigureApiBehaviorOptions(options =>
+    //{
+    //    options.SuppressConsumesConstraintForFormFileParameters = true;
+    //    options.SuppressInferBindingSourcesForParameters = true;
+    //    options.SuppressModelStateInvalidFilter = true;
+    //    options.SuppressMapClientErrors = true;
+    //    options.ClientErrorMapping[StatusCodes.Status404NotFound].Link =
+    //        "https://httpstatuses.com/404";
+    //});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors("Open");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
